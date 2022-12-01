3ae06690674226c6616b3c80ea0a476c7fea008f
Marat KH <336199@niuitmo.ru>
2022-12-01 06:05:20 +0300

    confused

---
+++ kekew.js
@@ -0,0 +1,7 @@
+/*
+Access-Control-Allow-Origin: *
+Content-Type: text/plain; charset=UTF-8
+X-Author: itmo336199
+Access-Control-Allow-Methods: GET,POST,DELETE,PUT,OPTIONS
+https-only: true
+/fetch/: Content-Type: text/html; charset-UTF-8;*/
