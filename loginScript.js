fa832abdb38c311410414ac3206d8925e5566d1a
Marat KH <336199@niuitmo.ru>
2022-12-01 05:48:23 +0300

    hmmm interesting

--- loginScript.js
+++
@@ -1,9 +0,0 @@
-function task(x) {
-    var lastChar = url.substr(-1); // Selects the last character
-if (lastChar != '/') {         // If the last character is not a slash
-   url = url + '/';            // Append a slash to it.
-}
-    x="itmo336199";
-    return x.toUpperCase();
-  }
-        
